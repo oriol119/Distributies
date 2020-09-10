891d775baa93789be945755d666d11c77d8a8207
Sagui10 <marc10sagues@gmail.com>
2020-09-10 14:03:18 +0200

    emulsiones in process

---
+++ js/open_options.js
@@ -0,0 +1,11 @@
+function emulsionantesOpen(){
+    emulsionantesOpenPhoto();
+}
+
+
+function emulsionantesOpenPhoto(){
+	var element = getElementsById('emulsifiers');
+
+	element.style.width = "100%";
+	element.style.height = "100%";
+}
